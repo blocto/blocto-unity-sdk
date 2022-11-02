99948a2a62d28f7944ee808940864842ec1461c9
jamisliao <jamisliao@portto.com>
2022-11-02 15:53:29 +0800

    feat: add value program

--- Assets/Script/SolanaController.cs
+++ Assets/Script/SolanaController.cs
@@ -1,17 +1,17 @@
-using System;
 using System.Collections.Generic;
-using System.Threading;
-using System.Threading.Tasks;
 using Blocto.Sdk.Core.Extension;
 using Blocto.Sdk.Core.Utility;
+using Blocto.Sdk.Solana;
 using Script.Model;
 using Solnet.Rpc;
+using Solnet.Rpc.Models;
+using Solnet.Wallet;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SolanaController : MonoBehaviour
 {
-    // private BloctoWalletProvider _bloctoWalletProvider;
+    private BloctoWalletProvider _bloctoWalletProvider;
     
     private Button _connectWalletBtn;
     
@@ -41,11 +41,7 @@ public class SolanaController : MonoBehaviour
         
         tmp = GameObject.Find("TestBtn");
         _testBtn = tmp.GetComponent<Button>();
-        _testBtn.onClick.AddListener(()=>{
-                                         var context = SynchronizationContext.Current;
-                                         var rest = GetTestEventWrapper(context).Result;
-                                         $"TestEnd.".ToLog();
-                                     });
+        _testBtn.onClick.AddListener(TestEvent);
         
         tmp = GameObject.Find("AccountTxt");
         _accountTxt = tmp.GetComponent<InputField>();
@@ -82,36 +78,38 @@ public class SolanaController : MonoBehaviour
         // _bloctoWalletProvider.ForceUseWebView = value;
     }
     
-    public async Task<string> GetTestEventWrapper(SynchronizationContext _context)
-    {
-        // var result = await GetRecentBlockIdAsync().ConfigureAwait(false);
-        // $"Block hash: {result}".ToLog();
-        
-        TestEvent(_context).ConfigureAwait(false).GetAwaiter().GetResult();
-        // _context.Post(_ =>{
-        //                   textMesh.text = "GetHtmlAsyncWrapper";
-        //               },null);
-        $"Test processed.".ToLog();
-        return "complete";
-    }
+    // public async Task<string> GetTestEventWrapper(SynchronizationContext _context)
+    // {
+    //     // var result = await GetRecentBlockIdAsync().ConfigureAwait(false);
+    //     // $"Block hash: {result}".ToLog();
+    //     
+    //     TestEvent(_context).ConfigureAwait(false).GetAwaiter().GetResult();
+    //     // _context.Post(_ =>{
+    //     //                   textMesh.text = "GetHtmlAsyncWrapper";
+    //     //               },null);
+    //     $"Test processed.".ToLog();
+    //     return "complete";
+    // }
     
-    private async Task TestEvent(SynchronizationContext context)
+    private void TestEvent()
     {
-        // var address = "CXxPxb5GAkqjVKxb3PkxFZmUus9YrTVuUoLWee4gm8ZR";
-        // var feePayer = new PublicKey("CXxPxb5GAkqjVKxb3PkxFZmUus9YrTVuUoLWee4gm8ZR");
-        // var blockHash = "5ado6ywC4oW6q5EMCSFKqr9CZ4EjrmAodxPUc1JHmUJt";
-        //
-        // var txInstruction = ValueProgram.CreateSetValaueInstruction(1111111, address);
-        // Transaction tx = new()
-        //                  {
-        //                      RecentBlockHash = blockHash,
-        //                      Instructions = new List<TransactionInstruction>{ txInstruction }
-        //                  };
-        //
-        // _bloctoWalletProvider.SignAndSendTransaction(address, tx);
         var solanaClient = ClientFactory.GetClient(Cluster.DevNet, _webRequestUtility);
-        var response = await solanaClient.GetLatestBlockHashAsync();
+        var response = solanaClient.GetLatestBlockHash();
         $"block hash: {response.Result.Value.Blockhash}".ToLog();
+        
+        var address = "CXxPxb5GAkqjVKxb3PkxFZmUus9YrTVuUoLWee4gm8ZR";
+        var feePayer = new PublicKey("CXxPxb5GAkqjVKxb3PkxFZmUus9YrTVuUoLWee4gm8ZR");
+        var blockHash = "5ado6ywC4oW6q5EMCSFKqr9CZ4EjrmAodxPUc1JHmUJt";
+        
+        var txInstruction = ValueProgram.CreateSetValaueInstruction(1111111, address);
+        Transaction tx = new()
+                         {
+                             RecentBlockHash = blockHash,
+                             FeePayer = feePayer,
+                             Instructions = new List<TransactionInstruction>{ txInstruction }
+                         };
+        
+        _bloctoWalletProvider.SignAndSendTransaction(address, tx);
     }
     
     // private async Task<string> GetRecentBlockIdAsync()
