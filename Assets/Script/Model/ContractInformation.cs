f01a72edb28b438f32bd523934c2c575bb72e576
jamisliao <jamisliao@portto.com>
2022-12-30 10:45:00 +0800

    feat: add blt contract information

---
+++ Assets/Script/Model/ContractInformation.cs
@@ -0,0 +1,9 @@
+namespace Script.Model
+{
+    public class ContractInformation
+    {
+        public string MainnetContractAddress { get; set; }
+
+        public string TestnetContractAddress { get; set; }
+    }
+}
