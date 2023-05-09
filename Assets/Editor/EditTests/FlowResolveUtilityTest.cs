2b864655d38f20945474d5fb0d985bcca3077c92
jamisliao <jamisliao@portto.com>
2023-05-09 14:05:20 +0800

    feat: complete first test

--- Assets/Editor/EditTests/FlowResolveUtilityTest.cs
+++ Assets/Editor/EditTests/FlowResolveUtilityTest.cs
@@ -1,7 +1,10 @@
 using System.Collections.Generic;
+using System.Linq;
 using Blocto.Sdk.Flow.Utility;
 using Flow.Net.Sdk.Core.Cadence;
 using Flow.Net.Sdk.Core.Models;
+using Newtonsoft.Json;
+using NSubstitute;
 using NUnit.Framework;
 
 namespace Editor.EditTests
@@ -19,26 +22,32 @@ namespace Editor.EditTests
         public void SetUp()
         {
             _target = new ResolveUtility();
-            _tx = new FlowTransaction
-                  {
-                      Script = _script,
-                      GasLimit = 1000,
-                      Arguments = new List<ICadence>
-                                  {
-                                      new CadenceNumber(CadenceNumberType.UFix64, $"0.1"),
-                                      new CadenceAddress("0xf90c31e986eb22f90xf90c31e986eb22f9")
-                                  },
-                  };
         }
 
         [Test]
-        public void TestResolve()
+        public void TestResolvePreSignable()
         {
-            var result = _target.ResolvePreSignable(ref _tx);
-            var expected = result.GetValue("interaction");
-            var expectInteraction = "";
+            var mockNumber = Substitute.For<CadenceNumber>(CadenceNumberType.UFix64, $"0.1");
+            mockNumber.TempId.Returns("1234567890");
             
-            Assert.AreEqual(result.GetValue("interaction"), expectInteraction);
+            var mockAddress = NSubstitute.Substitute.For<CadenceAddress>("0xf90c31e986eb22f90xf90c31e986eb22f9");
+            mockAddress.TempId.Returns("0987654321");
+            mockAddress.Type.Returns("Address");
+            
+            _tx = new FlowTransaction
+                  {
+                      Script = "test",
+                      GasLimit = 1000,
+                      Arguments = new List<ICadence>
+                                  {
+                                      mockNumber, mockAddress
+                                  },
+                  };
+            
+            var result = _target.ResolvePreSignable(ref _tx);
+            var actual = JsonConvert.SerializeObject(result);
+            var expected = "{\"args\":[{\"type\":\"UFix64\",\"value\":\"0.1\"},{\"type\":\"Address\",\"value\":\"0xf90c31e986eb22f90xf90c31e986eb22f9\"}],\"interaction\":{\"tag\":\"TRANSACTION\",\"assigns\":{},\"reason\":null,\"status\":\"OK\",\"arguments\":{\"1234567890\":{\"kind\":\"ARGUMENT\",\"tempId\":\"1234567890\",\"value\":\"0.1\",\"asArgument\":{\"type\":\"UFix64\",\"value\":\"0.1\"},\"xform\":{\"label\":\"UFix64\"}},\"0987654321\":{\"kind\":\"ARGUMENT\",\"tempId\":\"0987654321\",\"value\":\"0xf90c31e986eb22f90xf90c31e986eb22f9\",\"asArgument\":{\"type\":\"Address\",\"value\":\"0xf90c31e986eb22f90xf90c31e986eb22f9\"},\"xform\":{\"label\":\"Address\"}}},\"message\":{\"cadence\":\"test\",\"refBlock\":null,\"computeLimit\":1000,\"proposer\":null,\"payer\":null,\"authorizations\":[],\"arguments\":[\"1234567890\",\"0987654321\"],\"params\":[]},\"events\":null,\"account\":{\"addr\":null},\"collection\":null,\"transaction\":null,\"block\":null,\"params\":{}},\"voucher\":{\"cadence\":\"test\",\"refBlock\":null,\"computeLimit\":1000,\"arguments\":[{\"type\":\"UFix64\",\"value\":\"0.1\"},{\"type\":\"Address\",\"value\":\"0xf90c31e986eb22f90xf90c31e986eb22f9\"}],\"proposalKey\":{\"address\":null,\"keyId\":null,\"sequenceNum\":null}},\"f_type\":\"PreSignable\",\"f_vsn\":\"1.0.1\",\"addr\":null,\"keyId\":0,\"cadence\":\"test\",\"roles\":{\"proposer\":true,\"authorizer\":true,\"payer\":true}}";
+            Assert.AreEqual(expected, actual);
         }
     }
 }
